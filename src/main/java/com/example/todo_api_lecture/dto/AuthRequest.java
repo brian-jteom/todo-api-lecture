55f072daf15ed4a9eb438d316b26ac2df4cd5b2c
Eom Juntae <junbrian9815@gmail.com>
2024-08-26 19:12:10 +0900

    no message

---
+++ src/main/java/com/example/todo_api_lecture/dto/AuthRequest.java
@@ -0,0 +1,11 @@
+package com.example.todo_api_lecture.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthRequest {
+    private String username;
+    private String password;
+}
